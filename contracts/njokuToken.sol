14ecc3e14dc20ac39046cb98bd42e7ab2f021491
njokuScript <kalunjoku123@gmail.com>
2020-03-29 11:02:23 +0100

    declared variable

--- contracts/njokuToken.sol
+++ contracts/njokuToken.sol
@@ -1,11 +1,14 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-contract NjokuToken{
+
+contract NjokuToken {
     //constructor
     //set the total number of tokens
     //get the total number of tokn
-    function Token () public {
+    uint256 public totalSupply;
+
+    function Token() public {
         //store the number of tokens that will xist
-        totalSupply = 1000000
+        totalSupply = 1000000;
     }
 }
