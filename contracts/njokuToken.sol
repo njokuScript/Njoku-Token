e52ca9db8b050145334e33ff76674571a7960e2e
njokuScript <kalunjoku123@gmail.com>
2020-03-29 11:00:19 +0100

    created constructor

---
+++ contracts/njokuToken.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract NjokuToken{
+    //constructor
+    //set the total number of tokens
+    //get the total number of tokn
+    function Token () public {
+        //store the number of tokens that will xist
+        totalSupply = 1000000
+    }
+}
